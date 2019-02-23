c095d95f58e4706d2d131d8d012b5417f7d6ed52
Paco Benitez <paco.benitez.lazaro@gmail.com>
2019-02-23 13:21:19 +0100

    Some more tests

--- src/main/scala/example/Potter.scala
+++ src/main/scala/example/Potter.scala
@@ -1,14 +1,71 @@
 package example
 
+import example.Potter.discount
 
-case class Money(money: Int)
+import scala.collection.mutable.ArrayBuffer
+
+
+case class Money(money: Double)
 
 case class Book(book: Int)
 
+case class ShoppingCart() {
+
+  var books = new ArrayBuffer[Book]()
+
+  def size() = books.size
+
+  def append(book: Book) = books.append(book)
+
+  def haveSameBook(booksArgs: ArrayBuffer[Book]) = booksArgs.distinct.size != booksArgs.size
+
+  def costDifferent(booksArgs: ArrayBuffer[Book]): Money = {
+    assert(haveSameBook(booksArgs) == false)
+
+    var percent = 0
+
+    if(booksArgs.size == 1) percent = 0
+    if(booksArgs.size == 2) percent = 5
+    if(booksArgs.size == 3) percent = 10
+    if(booksArgs.size == 4) percent = 20
+    if(booksArgs.size == 5) percent = 25
+
+
+    Money(discount(8* booksArgs.size, percent ))
+  }
+
+
+}
+
 class Potter {
 
-  def cost(): Money = Money(8)
 
-  def addBook(book: Book) = {}
+  val books = ShoppingCart()
 
+  def cost(): Money = {
+    val groupBy = books.books.map(b =>
+      (books.books.count(b.equals(_)), b)
+    ).distinct
+
+    val conDosElem = groupBy.filter(_._1 == 2)
+    val conUnElem = groupBy.filter(_._1 >= 1)
+
+
+    Money( books.costDifferent(conDosElem.map(_._2)).money +
+      books.costDifferent(conUnElem.map(_._2)).money )
+
+  }
+
+
+  def addBook(book: Book) = {
+    books.append(book)
+  }
+
+}
+
+
+object Potter {
+  def discount(
+         number: Double,
+         percent: Double) = number * ( (100 - percent) / 100 )
 }
--- src/test/scala/example/PotterSpec.scala
+++ src/test/scala/example/PotterSpec.scala
@@ -9,4 +9,34 @@ class PotterSpec extends FlatSpec with Matchers {
     potter.cost() shouldEqual Money(8)
   }
 
+  "Two different books" should "cost 16 - 5%" in {
+    val potter = new Potter()
+    potter.addBook( Book(1) )
+    potter.addBook( Book(2) )
+    potter.cost() shouldEqual Money((8*2*95.0)/100)
+  }
+
+  "Two equals books" should "have no discount" in {
+    val potter = new Potter()
+    potter.addBook( Book(1) )
+    potter.addBook( Book(1) )
+    potter.cost() shouldEqual Money(8*2)
+  }
+
+  "Two equals + Other book" should "have one discount" in {
+    val potter = new Potter()
+    potter.addBook( Book(1) )
+    potter.addBook( Book(1) )
+    potter.addBook( Book(2) )
+    potter.cost() shouldEqual Money( 8 + (8*2*95.0)/100 )
+  }
+
+
+  "Three different books" should "cost 24 - 10%" in {
+    val potter = new Potter()
+    potter.addBook( Book(1) )
+    potter.addBook( Book(2) )
+    potter.addBook( Book(3) )
+    potter.cost() shouldEqual Money((8*3*90.0)/100)
+  }
 }
